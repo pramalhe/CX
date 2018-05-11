6692895ea1be45ad011a6a614f14d9b7dac8e653
Pedro Ramalhete <pramalhe@gmail.com>
2018-05-11 15:55:21 +0200

    int instead of k

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -2,7 +2,7 @@
 #include <set>
 
 int main(void) {
-    // Create a CX which wraps a std::set<K> and initializes it with a new (empty) instance
+    // Create a CX which wraps a std::set<int> and initializes it with a new (empty) instance
     CXMutationWF<std::set<int>> cx {new std::set<int>()};
 
     int key = 33;
